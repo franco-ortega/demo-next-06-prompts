141a8db6e07764aae575bee97456c81abb9cbd4d
Franco Ortega <francopdx@gmail.com>
2023-06-19 16:22:12 -0700

    create planets page

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
+import Link from 'next/link';
 import './globals.css';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'NextJS Demo',
@@ -11,7 +12,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+      // className={inter.className}
+      >
+        <header>
+          <h1 className='text-xl text-gray-600 w-fit m-auto'>Header</h1>
+          <nav className='w-fit m-auto text-blue-500'>
+            <ul className='flex flex-row gap-2'>
+              <li>
+                <Link href={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link href={'/planets'}>Planets</Link>
+              </li>
+            </ul>
+          </nav>
+          <hr />
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
