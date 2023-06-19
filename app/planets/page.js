05b6d1b7265843129311c8b1314a6c566c24c9ac
Franco Ortega <francopdx@gmail.com>
2023-06-19 16:43:04 -0700

    create layout for planets page

--- app/planets/page.js
+++ app/planets/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Planets() {
   return (
-    <main>
+    <>
       <h2>Planets</h2>
       <ul className='flex flex-col gap-1 px-2'>
         <li>
@@ -12,6 +12,6 @@ export default function Planets() {
           <Link href='/planets/venus'>Venus</Link>
         </li>
       </ul>
-    </main>
+    </>
   );
 }
