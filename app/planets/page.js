7479ca527268a9cc9dcc6975ebd84e286c6fed33
Franco Ortega <francopdx@gmail.com>
2023-06-19 16:57:17 -0700

    dynamically create planet list

--- app/planets/page.js
+++ app/planets/page.js
@@ -1,16 +1,28 @@
+'use client';
+
 import Link from 'next/link';
+import planetsData from '../data/planets';
+import { useEffect, useState } from 'react';
 
 export default function Planets() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    setPlanets(planetsData);
+  }, []);
+
+  console.log(planets);
+
   return (
     <>
       <h2>Planets</h2>
       <ul className='flex flex-col gap-1 px-2'>
-        <li>
-          <Link href='/planets/mercury'>Mercury</Link>
-        </li>
-        <li>
-          <Link href='/planets/venus'>Venus</Link>
-        </li>
+        {planets.length > 0 &&
+          planets.map((planet) => (
+            <li key={planet.id}>
+              <Link href={`/planets/${planet.path}`}>{planet.title}</Link>
+            </li>
+          ))}
       </ul>
     </>
   );
