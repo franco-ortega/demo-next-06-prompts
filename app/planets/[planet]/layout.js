07d96b20592759cb33aa34aca83170cf52d64ed3
Franco Ortega <francopdx@gmail.com>
2023-06-26 17:01:36 -0700

    make the planet round

--- app/planets/[planet]/layout.js
+++ app/planets/[planet]/layout.js
@@ -1,8 +1,13 @@
 export default function PlanetLayout({ children }) {
   return (
     <main className='bg-blue-700 text-cyan-300 w-fit mx-auto my-5 p-10 rounded-full'>
+      <p className='mx-auto w-fit'>.</p>
+      <p className='mx-auto w-fit'>.</p>
+      <p className='mx-auto w-fit'>.</p>
       {children}
-      <p className='bg-sky-500 text-sm p-5'>Layout in planet.</p>
+      <p className='mx-auto w-fit'>.</p>
+      <p className='mx-auto w-fit'>.</p>
+      <p className='mx-auto w-fit'>.</p>
     </main>
   );
 }
