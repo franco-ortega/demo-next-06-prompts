5ac742fbaf93049a44b62cf92707b8eb03b837b2
Franco Ortega <francopdx@gmail.com>
2023-06-26 17:03:08 -0700

    make height full screen

--- app/planets/layout.js
+++ app/planets/layout.js
@@ -1,6 +1,6 @@
 export default function PlanetsLayout({ children }) {
   return (
-    <main className='bg-blue-900 text-cyan-300'>
+    <main className='bg-blue-900 text-cyan-300 min-h-screen'>
       <p className='bg-sky-950 text-sm p-5'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
