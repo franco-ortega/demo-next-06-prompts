b7f023dc3f557a346f9b9ca6d5ee45cb02128fc7
Franco Ortega <francopdx@gmail.com>
2023-06-30 17:26:29 -0700

    replace lorem in global layout with lorem from data file

--- app/planets/layout.js
+++ app/planets/layout.js
@@ -1,15 +1,9 @@
+import lorem from '../../data/lorem';
+
 export default function PlanetsLayout({ children }) {
   return (
     <main className='bg-blue-900 text-cyan-300 min-h-screen'>
-      <p className='bg-sky-950 text-sm p-5'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <p className='bg-sky-950 text-sm p-5'>{lorem.medium}</p>
 
       {children}
     </main>
