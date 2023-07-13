79b5d5f60ca8806f632cf06dcef03ff386d842e9
Franco Ortega <francopdx@gmail.com>
2023-07-13 16:19:25 -0700

    refactor home styles into object

--- app/page.js
+++ app/page.js
@@ -2,9 +2,15 @@ import lorem from '../data/lorem';
 
 export default function Home() {
   return (
-    <main className='bg-blue-500 w-96 m-auto p-5'>
-      <h2 className='p-2'>Hello World</h2>
-      <p className='bg-blue-700 p-2'>{lorem.short}</p>
+    <main className={styles.main}>
+      <h2 className={styles.h2}>Hello World</h2>
+      <p className={styles.p}>{lorem.short}</p>
     </main>
   );
 }
+
+const styles = {
+  main: 'bg-blue-500 w-96 m-auto p-5',
+  h2: 'p-2',
+  p: 'bg-blue-700 p-2',
+};
