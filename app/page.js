824db4e4ac7e61f509d7af1cb4880d65098bf87b
Franco Ortega <francopdx@gmail.com>
2023-07-13 16:17:23 -0700

    add styles to home page

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import lorem from '../data/lorem';
 
 export default function Home() {
   return (
-    <main>
-      <h2>Hello World</h2>
-      <p>{lorem.short}</p>
+    <main className='bg-blue-500 w-96 m-auto p-5'>
+      <h2 className='p-2'>Hello World</h2>
+      <p className='bg-blue-700 p-2'>{lorem.short}</p>
     </main>
   );
 }
